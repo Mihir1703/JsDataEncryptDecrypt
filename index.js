7fe9f7e18776e06127f349f69f6a3b654507b95e
Mihir Waykole <76737051+Mihir1703@users.noreply.github.com>
2022-06-06 17:29:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 const encrypt = require('./Util/encrypt');
-const decrypt = require('./Util/decrypt');
 const app = express();
 const port = 3000;
 app.use(cors());
